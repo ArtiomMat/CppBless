1c43113d57af6e07ba34b357fabe2c439bf278e1
doomguy <artiomat13100@gmail.com>
2024-08-30 22:51:58 +0300

    first commit

---
+++ CMakeFiles/Bless.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/doomguy/Projects/Bless/src/Terminal.cpp" "CMakeFiles/Bless.dir/src/Terminal.cpp.o" "gcc" "CMakeFiles/Bless.dir/src/Terminal.cpp.o.d"
+  "/home/doomguy/Projects/Bless/src/linux/Terminal.cpp" "CMakeFiles/Bless.dir/src/linux/Terminal.cpp.o" "gcc" "CMakeFiles/Bless.dir/src/linux/Terminal.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Bless.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Bless.
+# This may be replaced when dependencies are built.
